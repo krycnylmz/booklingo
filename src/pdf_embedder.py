222a4f9ebf5ce60d72731ee350f41a9aca24399b
Koray Can Yılmaz <43735441+krycnylmz@users.noreply.github.com>
2025-10-22 22:01:54 +0300

    feature(pdf-embedding): change the pdf to text lib to get better results

--- src/pdf_embedder.py
+++ src/pdf_embedder.py
@@ -1,5 +1,5 @@
 import os
-from PyPDF2 import PdfReader
+from pypdfium2 import PdfDocument
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import google.generativeai as genai
 import chromadb
@@ -17,10 +17,11 @@ embedding_model = os.getenv("EMBEDDING_MODEL", "models/text-embedding-004")
 
 # ── functions
 def embed_pdf(pdf_path):
-    reader = PdfReader(pdf_path)
+    pdf = PdfDocument(pdf_path)
     text = ""
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
+    for page in pdf:
+        page_text = page.get_textpage().get_text_range()
+        text += (page_text or "") + "\n"
 
     splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=120)
     chunks = splitter.split_text(text)
